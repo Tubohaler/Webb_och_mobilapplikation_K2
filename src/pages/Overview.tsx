a95c104355097dd0746f43e77989fa5911e8e6a1
David Skotte <david.skotte@gmail.com>
2022-11-22 20:17:12 +0100

    polished the styling a bit for Overview

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -13,16 +13,14 @@ const StyledWrapper = styled.section`
 
 const StyledLogo = styled.img<Props>`
   width: 10rem;
-  position: fixed;
+  position: static;
   top: 0;
   margin-left: 7rem;
   margin-top: 1rem;
 `;
 
 const OverviewBox = styled.div`
-  position: fixed;
   top: 0;
-  margin-top: 5rem;
   width: 90vw;
   height: 18rem;
 
@@ -38,17 +36,15 @@ const OverviewBox = styled.div`
 `;
 
 const OverviewTitle = styled.h2`
-  position: fixed;
+  position: static;
   top: 0;
-  margin-top: 3.4rem;
+  margin-top: rem;
   margin-left: 1.2rem;
 
   font-size: 1.3rem;
 `;
 
-const OverviewProjectsTitle = styled.h5`
-  margin-left: 2rem;
-`;
+const OverviewProjectsTitle = styled.h5``;
 
 const OverviewProjectsSmallBox = styled.div`
   width: 6rem;
@@ -83,8 +79,7 @@ const OverviewInvoiceSmallBox = styled.div`
 `;
 
 const ProjectsBox = styled.div`
-  position: absolute;
-  margin-top: 5rem;
+  position: static;
   margin-left: 1.2rem;
 
   width: 90vw;
@@ -94,11 +89,61 @@ const ProjectsBox = styled.div`
 `;
 
 const ProjectsTitle = styled.h2`
-  position: fixed;
+  position: static;
   top: 0;
-  margin-top: 24rem;
   margin-left: 1.2rem;
   font-size: 1.3rem;
+`;
+
+const TasksTitle = styled.h2`
+  position: static;
+  top: 0;
+  margin-left: 1.2rem;
+  font-size: 1.3rem;
+`;
+
+const TasksBox = styled.div`
+  position: static;
+  margin-left: 1.2rem;
+
+  width: 90vw;
+  height: 10rem;
+  background: #126923;
+  border-radius: 35px;
+`;
+
+const TimerTitle = styled.h2`
+  position: static;
+  top: 0;
+  margin-left: 1.2rem;
+  font-size: 1.3rem;
+`;
+
+const TimerBox = styled.div`
+  position: static;
+  margin-left: 1.2rem;
+
+  width: 90vw;
+  height: 10rem;
+  background: #b53a4d;
+  border-radius: 35px;
+`;
+
+const InvoicesTitle = styled.h2`
+  position: static;
+  top: 0;
+  margin-left: 1.2rem;
+  font-size: 1.3rem;
+`;
+
+const InvoicesBox = styled.div`
+  position: static;
+  margin-left: 1.2rem;
+
+  width: 90vw;
+  height: 10rem;
+  background: #5c28b5;
+  border-radius: 35px;
 `;
 
 export default function Overview({}: Props) {
@@ -108,16 +153,20 @@ export default function Overview({}: Props) {
         <StyledLogo src={logo} className="logo" alt="logo" />
       </div>
       <OverviewTitle>Overview</OverviewTitle>
-      <OverviewBox className="overview">
-        <OverviewProjectsSmallBox className="overviewProjects">
-          <OverviewProjectsTitle>Projects</OverviewProjectsTitle>
-        </OverviewProjectsSmallBox>
+      <OverviewBox>
+        <OverviewProjectsSmallBox>Projects</OverviewProjectsSmallBox>
         <OverviewTasksSmallBox>Tasks</OverviewTasksSmallBox>
         <OverviewTimerSmallBox>Timer</OverviewTimerSmallBox>
         <OverviewInvoiceSmallBox>Invoice</OverviewInvoiceSmallBox>
       </OverviewBox>
       <ProjectsTitle>Projects</ProjectsTitle>
-      <ProjectsBox className="projectsBox"></ProjectsBox>
+      <ProjectsBox></ProjectsBox>
+      <TasksTitle>Tasks</TasksTitle>
+      <TasksBox></TasksBox>
+      <TimerTitle>Timer</TimerTitle>
+      <TimerBox></TimerBox>
+      <InvoicesTitle>Invoices</InvoicesTitle>
+      <InvoicesBox></InvoicesBox>
     </StyledWrapper>
   );
 }
